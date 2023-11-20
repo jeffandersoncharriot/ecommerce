d83af34b77d2e0c3708bd0b1ea2fe0887b6d3ffd
jeffandersoncharriot <98402030+jeffandersoncharriot@users.noreply.github.com>
2023-11-20 13:26:55 -0500

    solution added

---
+++ ECommerce.Api.Products.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace ECommerce.Api.Products.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int s = 1;
+
+            Assert.Equal(1, s);
+        }
+    }
+}
